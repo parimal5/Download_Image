0638eff9cc9f2e24d25b794d60e1d9df66cfd64d
parimal5 <parimalmeshram15@gmail.com>
2021-05-21 00:18:41 +0530

    Adding Image.

--- app/src/main/java/com/example/downloadimage/MainActivity.java
+++ app/src/main/java/com/example/downloadimage/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.downloadimage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+
+    public void downloadImage(View view){
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
